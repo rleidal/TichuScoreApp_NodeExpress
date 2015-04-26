e255debbe93df4d08f8329f214a08aa0b5c3abf4
Ryan <ryan.leidal@gmail.com>
2015-04-26 00:22:05 -0400

    Added some base grunt stuff to get that going

---
+++ TichuScoreApp_NodeExpress/Gruntfile.js
@@ -0,0 +1,51 @@
+﻿/*global module */
+    'use strict';
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // read in the project settings from the package.json file into the pkg property
+        pkg: grunt.file.readJSON('package.json')
+
+        // define configuration for each of the tasks we have
+        // this is a sample jshint task config
+        /*
+            jshint: {
+                    // define the files to lint
+                    files: ['gruntfile.js', 'src/*.js', '/*.js'],
+                    // configure JSHint (documented at http://www.jshint.com/docs/)
+                    options: {
+                    // more options here if you want to override JSHint defaults
+                    globals: {
+                            jQuery: true,
+                            console: true,
+                            module: true
+                            }
+                        }
+                    }
+        */
+    });
+
+    // Add all plugins that your project needs here
+//    grunt.loadNpmTasks('');
+
+    // this would be run by typing "grunt test" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('test', []);
+
+    // define the default task that can be run just by typing "grunt" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('default', []);
+
+    grunt.registerTask('buildDeploy', 'Sample tast that finds all the js files', function () {
+        mvJsFiles();
+    });
+
+    
+    function mvJsFiles() {
+        var i = 0;
+        var files = grunt.file.expand({matchBase:true},['*.js', '!node_modules/**']);
+        for (i = 0; i < files.length; i++) {
+            grunt.log.writeln("File: " + files[i]);
+        }
+    }
+};
+
