e98c9f232c38942dc9474e33f5b669fc8957832a
Ryan <ryan.leidal@gmail.com>
2015-05-14 00:20:33 -0400

    Updated styles and scoresheet

--- TichuScoreApp_NodeExpress/public/javascripts/tichuClient.ts
+++ TichuScoreApp_NodeExpress/public/javascripts/tichuClient.ts
@@ -1,35 +1,58 @@
 ﻿class teamHand {
     tichuL: string;
     tichuR: string;
-    totalPts: number;
+    cardPts: number;
+    contructor() {
+        this.tichuL = "";
+        this.tichuR = "";
+        this.cardPts = 0;
+    }
+    get totalPts(): number {
+        var pts: number = this.cardPts;
+
+        pts += (this.tichuL == 'G') ? 200 :
+            (this.tichuL == '!G') ? -200 :
+                (this.tichuL == 'T') ? 100 :
+                    (this.tichuL == '!T') ? -100 : 0;
+        pts += (this.tichuR == 'G') ? 200 :
+            (this.tichuR == '!G') ? -200 :
+                (this.tichuR == 'T') ? 100 :
+                    (this.tichuR == '!T') ? -100 : 0;
+
+        return pts;
+    }
 }
 
 class team {
     nameL: string;
     nameR: string;
     score: number;
+    id: number;
     hands: teamHand[];
+    handIn: teamHand;
     constructor(l: string, r:string) {
         this.nameL = l;
         this.nameR = r;
         this.score = 0;
-//        this.score = 5 * Math.floor(Math.random() * 200);
         this.hands = [];
+        this.id = 0;
+        this.handIn = new teamHand();
     }
     addHand(h: teamHand) {
         this.score += h.totalPts;
         this.hands.push(h);
     }
+    store() {
+        this.addHand(this.handIn);
+        this.handIn = new teamHand();
+    }
 }
 
 interface tichuScoreSheetView extends ng.IScope {
     msg_list: string[];
-    sum: number;
 
     teams: team[];
 
-    leftStyle: any;
-    rightStyle: any;
     events: tichuScoreClientController;
 }
 
@@ -48,56 +71,28 @@ class tichuScoreClientController {
     init_sample() {
         var x: number;
 
-        this.view.sum = 6;
         this.view.teams = [];
         this.view.teams.push(new team("Fran", "Steve"));
         this.view.teams.push(new team("Ryan", "Mark"));
-        this.view.leftStyle = { width: '15%', float: 'left', height: '20px' };
-        this.view.rightStyle = { 'margin-left': '15%', height: '20px' };
-
-        for (x = 0; x < 6; x++) {
-            var scoreA, scoreB: number;
-            scoreA = 5 * Math.floor(Math.random() * 30) - 25;
-            scoreB = 100 - scoreA;
-            this.view.teams[0].addHand({ totalPts: scoreA, tichuL: this.pickTS(), tichuR: this.pickTS() });
-            this.view.teams[1].addHand({ totalPts: scoreB, tichuL: this.pickTS(), tichuR: this.pickTS() });
-        }
+        this.view.teams[0].id = 0;
+        this.view.teams[1].id = 1;
     }
-
-    pickTS(): string {
-        var s: string = "";
-        var r: number = Math.floor(Math.random() * 25);
-        switch (r) {
-            case 0,1: s = "!G"; break;
-            case 2,3,4,5: s = "G"; break;
-            case 6,7,8: s = "!T"; break;
-            case 9,10,11,12,13,14,15: s = "T"; break;
-        }
-        return s;
+    go() {
+        this.view.teams[0].store();
+        this.view.teams[1].store();
     }
-
     swap() {
         var temp: team = this.view.teams[0];
         this.view.teams[0] = this.view.teams[1];
         this.view.teams[1] = temp;
-//        temp = this.view.teams.shift();
-//        this.view.teams.push(temp);
+    }
+    updatePts(i: number) {
+        var pts_i: number = this.view.teams[i].handIn.cardPts;
+        var pts_other: number = 0;
+
+        if (200 != pts_i) {
+            pts_other = 100 - pts_i;
+        }
+        this.view.teams[i ^ 1].handIn.cardPts = pts_other;
     }
 }
-
-    function userMsg(mtype: string, msg: string) {
-        $("<div class='" + mtype + "'>").text(msg).appendTo("#log");
-    }
-
-    var errorMsg = userMsg.bind(null, "errorMsg");
-    var warnMsg = userMsg.bind(null, "warnMsg");
-    var infoMsg = userMsg.bind(null, "infoMsg");
-
-    function submitScore() {
-    }
-
-    function requestScoreKeeper() {
-    }
-
-    function swapSides() {
-    }
