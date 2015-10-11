5256cc4d522e56c9a18afea0a864933d6a5edd2a
Ryan Leidal <ryan.leidal@gmail.com>
2015-10-10 23:37:43 -0400

    Fixed up back to compile on 2015

---
+++ TichuScoreApp_NodeExpress/routes/serverSocket.ts
@@ -0,0 +1,29 @@
+﻿var baseSocket = require('socket.io/lib/socket');
+
+//var this: tichuCommunications.serverSocket;
+
+baseSocket.prototype.updateScore = 
+function (score_data: tichuInterfaces.gameUpdate_intf): void {
+    this.broadcast.to(this.currentGame).emit("s2c_UpdateScore", score_data);
+}
+
+baseSocket.prototype.genRoomStr =
+function (id: number): string {
+    return "GameRoom_" + id;
+}
+
+baseSocket.prototype.enterGame =
+function (id: number): void {
+    console.log("Entering game " + id);
+    if (this.currentGame) {
+        this.leave(this.currentGame);
+    }
+    this.gameID = id;
+    this.currentGame = this.genRoomStr(id);
+    this.join(this.currentGame);
+}
+
+function ssUpdate (): void {
+}
+
+export = ssUpdate;
